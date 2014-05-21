5dbce036100350509e54c8df122e3cc2a4c9be30
Shaun Luttin <shaunluttin@gmail.com>
2014-05-20 18:08:38 -0700

    Add an output cache attribute to home.

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        [OutputCache(Location=OutputCacheLocation.Client, Duration=0)]
         public ActionResult Index()
         {
             return View();
